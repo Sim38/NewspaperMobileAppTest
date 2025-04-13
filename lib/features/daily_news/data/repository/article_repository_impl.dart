ebc297fa4f8eddbd1789108ddcc2f316a01d5f7a
Sim38 <simjunhong38@gmail.com>
2025-04-13 11:04:55 +0800

    Implemented article_repository with ArticleModel

---
+++ lib/features/daily_news/data/repository/article_repository_impl.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:learning_application/core/resources/data_state.dart';
+import 'package:learning_application/features/daily_news/data/data_sources/remote/news_api_service.dart';
+import 'package:learning_application/features/daily_news/data/models/article.dart';
+import 'package:learning_application/features/daily_news/domain/entities/article.dart';
+import 'package:learning_application/features/daily_news/domain/repository/article_repository.dart';
+
+class ArticleRepositoryImpl implements ArticleRepository {
+  final NewsApiService _newsApiService;
+  ArticleRepositoryImpl(this._newsApiService);
+
+  @override
+  Future<DataState<List<ArticleModel>>> getNewsArticle() async {
+    try {
+      final httpResponse = await _newsApiService.getNewsArticles(
+        apiKey: dotenv.env["NEWS_API_KEY"],
+      );
+
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataFailed(
+          DioException(
+            error: httpResponse.response.statusMessage,
+            requestOptions: httpResponse.response.requestOptions,
+            response: httpResponse.response,
+            type: DioExceptionType.badResponse,
+          ),
+        );
+      }
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
+  }
+}

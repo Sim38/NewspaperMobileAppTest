c2926e2dec6f93a34c1b2baa1369f23d545e75a4
Sim38 <simjunhong38@gmail.com>
2025-04-13 11:03:24 +0800

    Added abstract Repository in domain
    To be implemented by repository in Data Layer

---
+++ lib/features/daily_news/domain/repository/article_repository.dart
@@ -0,0 +1,6 @@
+import 'package:learning_application/core/resources/data_state.dart';
+import 'package:learning_application/features/daily_news/domain/entities/article.dart';
+
+abstract class ArticleRepository {
+  Future<DataState<List<ArticleEntity>>> getNewsArticle();
+}
